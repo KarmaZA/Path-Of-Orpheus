9b5a9add726f1598037e69ffcdde73138848be39
KarmaZA <EVRJON003@myuct.ac.za>
2020-05-15 21:26:38 +0200

    Added a sphere
    Sphere hovers for a set time and then falls;

---
+++ Assets/HoverTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+//using UnityEngine.UI;
+
+
+public class HoverTimer : MonoBehaviour
+{
+    public Collider sphere;
+    public int waitTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+        
+    }
+
+    // Update is called once per frame
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(waitTime);
+        sphere.attachedRigidbody.useGravity =true;
+
+    }
+}
